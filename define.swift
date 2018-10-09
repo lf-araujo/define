b124c7b0ccf4074b907b73d685c0a6f4619176c4
luis <ldearaujo@unimelb.edu.au>
2018-10-09 19:53:33 -0300

    Added synonyms and antonyms.

--- define.swift
+++ define.swift
@@ -6,11 +6,13 @@ let doc: String = """
 Get definitions from the Oxford Dictionary right from the command line.
 
 Usage:
- define WORD [-l LANG]
+ define WORD [-l LANG] [-as]
 
 Options:
- -l LANG --lang LANG  [default: en]
- -h --help            Shows this screen
+ -l LANG --lang LANG  [default: en]   Language (es, gu, hi, lv, sw, ta)
+ -a --antonym                         Get antonyms for WORD
+ -s --synonym                         Get synonyms for WORD
+ -h --help                            Shows this screen
  --version
 
 """
@@ -25,18 +27,16 @@ let group = DispatchGroup.init()
 let appId = "8a7dd147"
 let appKey = "7e7d022fbce6d8d4523eac3baa5bd04c"
 
-let url = URL(string: "https://od-api.oxforddictionaries.com/api/v1/entries/\(argument["--lang"]!)/\(argument["WORD"]!)")!
-var request = URLRequest(url: url)
-request.addValue("application/json", forHTTPHeaderField: "Accept")
-request.addValue(appId, forHTTPHeaderField: "app_id")
-request.addValue(appKey, forHTTPHeaderField: "app_key")
-
-group.enter() // Use this before making anything that needs to be waited for
-              // This manually add one to operation count in the dispatch group
-URLSession.shared.dataTask(with: request, completionHandler: { data, response, error -> Void in
+func definition() {
+  let url = URL(string:
+  "https://od-api.oxforddictionaries.com/api/v1/entries/\(argument["--lang"]!)/\(argument["WORD"]!)")!
+  var request = URLRequest(url: url)
+  request.addValue("application/json", forHTTPHeaderField: "Accept")
+  request.addValue(appId, forHTTPHeaderField: "app_id")
+  request.addValue(appKey, forHTTPHeaderField: "app_key")
+  URLSession.shared.dataTask(with: request, completionHandler: { data, response, error -> Void in
     defer {  // Defer makes all ends of this scope make something, here we want to leave the dispatch.
              // This is executed when the scope ends, even if with exception.
-
        group.leave() // Manually subtract one from the operation count
     }
 
@@ -52,6 +52,77 @@ URLSession.shared.dataTask(with: request, completionHandler: { data, response, e
         print(error)
         print(NSString.init(data: data!, encoding: String.Encoding.utf8.rawValue))
     }
-}).resume()
+    }).resume()
+}
+
+func antonyms() {
+  let url = URL(string:
+    "https://od-api.oxforddictionaries.com/api/v1/entries/\(argument["--lang"]!)/\(argument["WORD"]!)/antonyms")!
+  var request = URLRequest(url: url)
+  request.addValue("application/json", forHTTPHeaderField: "Accept")
+  request.addValue(appId, forHTTPHeaderField: "app_id")
+  request.addValue(appKey, forHTTPHeaderField: "app_key")
+  URLSession.shared.dataTask(with: request, completionHandler: { data, response, error -> Void in
+    defer {  // Defer makes all ends of this scope make something, here we want to leave the dispatch.
+             // This is executed when the scope ends, even if with exception.
+       group.leave() // Manually subtract one from the operation count
+    }
+
+    if let response = response,
+        let data = data,
+        let jsonData = try? JSON(data: data) {
+           for (key, subJson): (String, JSON) in jsonData["results"][0]["lexicalEntries"] {
+              //print(key)
+              print(subJson["entries"][0]["senses"][0]["antonyms"].arrayValue.map({$0["text"].stringValue})
+                .map {String($0)} .joined(separator: "\n"))
+            }
+    } else {
+        print(error)
+        print(NSString.init(data: data!, encoding: String.Encoding.utf8.rawValue))
+    }
+  }).resume()
+}
+
+func synonym() {
+    let url = URL(string:
+    "https://od-api.oxforddictionaries.com/api/v1/entries/\(argument["--lang"]!)/\(argument["WORD"]!)/synonyms")!
+  var request = URLRequest(url: url)
+  request.addValue("application/json", forHTTPHeaderField: "Accept")
+  request.addValue(appId, forHTTPHeaderField: "app_id")
+  request.addValue(appKey, forHTTPHeaderField: "app_key")
+  URLSession.shared.dataTask(with: request, completionHandler: { data, response, error -> Void in
+    defer {  // Defer makes all ends of this scope make something, here we want to leave the dispatch.
+             // This is executed when the scope ends, even if with exception.
+       group.leave() // Manually subtract one from the operation count
+    }
+
+    if let response = response,
+        let data = data,
+        let jsonData = try? JSON(data: data) {
+           for (key, subJson): (String, JSON) in jsonData["results"][0]["lexicalEntries"] {
+              //print(key)
+              print(subJson["entries"][0]["senses"][0]["synonyms"].arrayValue.map({$0["text"].stringValue})
+                .map {String($0)} .joined(separator: "\n"))
+
+            }
+    } else {
+        print(error)
+        print(NSString.init(data: data!, encoding: String.Encoding.utf8.rawValue))
+    }
+  }).resume()
+}
+
+group.enter() // Use this before making anything that needs to be waited for
+              // This manually add one to operation count in the dispatch group
+
+scope: if argument["--antonym"] as! Bool {
+  antonyms()
+  break scope
+} else if argument["--synonym"] as! Bool {
+  synonym()
+  break scope
+} else {
+  definition()
+}
 
 group.wait()  // Wait for group to end operations.
